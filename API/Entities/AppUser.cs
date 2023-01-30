e589e0cabff4cf00f44022675aa892ad0d6ee0c0
arun <arunkumar12712@gmail.com>
2023-01-31 01:20:22 +0530

    create seperate class ofbasecontroller and inherite other controller

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,7 @@ namespace API.Entities
     {
         public int id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt{ get; set; }
     }
 }
