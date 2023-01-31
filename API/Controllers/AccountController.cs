2a0424146e77def1297e5dd17e22e175e910f965
arun <arunkumar12712@gmail.com>
2023-01-31 21:14:23 +0530

    login endpoint and client validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,6 +36,23 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if(user == null) return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(t => t.UserName == username.ToLower());
