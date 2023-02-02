07ce34751f2f2bc9edcd8f9e48dff746556ddefd
arun <arunkumar12712@gmail.com>
2023-02-03 00:53:31 +0530

    improve login function

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,24 +11,36 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  loggedIn = false;
+  //loggedIn: false;
+  currentUser$: Observable<User| null> = of(null);
 
   constructor( private accountService: AccountService) { }
 
   ngOnInit(): void {
+    //this.getCurrentUser();
+    this.currentUser$= this.accountService.currentUser$;
   }
+
+  // getCurrentUser(){
+  //   this.accountService.currentUser$.subscribe({
+  //     next: user => this.loggedIn = !!user,
+  //     error: error=> console.log(error)
+  //   })
+  // }
+
 
   login(){
     this.accountService.login(this.model).subscribe({
       next:response=>{
         console.log(response);
-        this.loggedIn = true;
+        //this.loggedIn = true;
       },
       error: error=>console.log(error)
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
+    //this.loggedIn = false;
   }
 }
