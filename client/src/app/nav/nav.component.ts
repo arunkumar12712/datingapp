7b758ce09c03fff4ec7c476733df11ccbeddbf0f
arun <arunkumar12712@gmail.com>
2023-02-05 19:33:51 +0530

    exception handling client and web api

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -39,7 +39,7 @@ export class NavComponent implements OnInit {
       //   console.log(response);
       //   //this.loggedIn = true;
       // },
-      error: error=>this.toastrService.error(error.error)
+
     })
   }
 
